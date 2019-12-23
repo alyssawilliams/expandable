ee36b6c8e7df74999accf9bdaabce08b42995d8c
Alyssa Williams <awilliamsdev@gmail.com>
2019-12-23 18:50:09 -0500

    Styling Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import menu from '../../src/images/menu.svg';
 
 export default function Header() {
   return (
-    <header>Header</header>
+    <header className="header">
+      <button className="header__button">
+        <img src={menu} alt="Expand menu"/>
+      </button>
+      <span className="header__text">Expandable Content</span>
+    </header>
   )
 }
