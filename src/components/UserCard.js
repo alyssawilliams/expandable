629f85107ab7eb9ce73f19ea0775f096e7441d05
Alyssa Williams <awilliamsdev@gmail.com>
2019-12-23 23:21:19 -0500

    Styling accordion

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -7,7 +7,7 @@ export default function Usercard(props) {
   return (
     <div className="user__card">
       <div className="user__top">
-        <img className="user__photo" src={picture} alt={`${name}'s profile picture`} />
+        <img className="user__photo" src={picture} alt={name} />
         <div className="user__data">
           <div className="user__name">{name}</div>
           <div className="user__location">{location}</div>
@@ -18,8 +18,14 @@ export default function Usercard(props) {
       </div>
 
       <div className="accordion">
-        <div>Phone {phone}</div>
-        <div>Email {email}</div>
+        <div className="user__phone">
+          <div className="label">Phone</div> 
+          <div className="data">{phone}</div>
+        </div>
+        <div className="user__email">
+          <div className="label">Email</div> 
+          <div className="data">{email}</div>
+        </div>
       </div>
     </div>
   );
