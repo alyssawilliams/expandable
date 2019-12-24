fb1eb19495ab9d33961b28485cefae800ea99a57
Alyssa Williams <awilliamsdev@gmail.com>
2019-12-24 09:50:18 -0500

    Renaming variables

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -8,23 +8,23 @@ export default function Usercard(props) {
     <div className="user__card">
       <div className="user__top">
         <img className="user__photo" src={picture} alt={name} />
-        <div className="user__data">
+        <div className="user__text">
           <div className="user__name">{name}</div>
           <div className="user__location">{location}</div>
         </div>
-        <button className="accordion__button">
-          <img className="accordion__arrow" src={arrow} alt="Show contact info"/>
+        <button className="user__accordion__button">
+          <img className="user__accordion__arrow" src={arrow} alt="Show contact info"/>
         </button>
       </div>
 
-      <div className="accordion">
-        <div className="user__phone">
-          <div className="label">Phone</div> 
-          <div className="data">{phone}</div>
+      <div className="user__accordion">
+        <div className="user__contact user__contact--phone">
+          <div className="user__label">Phone</div> 
+          <div className="user__data">{phone}</div>
         </div>
-        <div className="user__email">
-          <div className="label">Email</div> 
-          <div className="data">{email}</div>
+        <div className="user__contact user__contact--email">
+          <div className="user__label">Email</div> 
+          <div className="user__data">{email}</div>
         </div>
       </div>
     </div>
