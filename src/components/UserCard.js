ea19a22422a7744ba4dc326120a581e58d48f8c3
Alyssa Williams <awilliamsdev@gmail.com>
2019-12-23 21:41:24 -0500

    Styling user card top

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import arrow from '../../src/images/arrow.svg';
 
 export default function Usercard(props) {
   const { picture, name, location, phone, email } = props; 
 
   return (
-    <div className="user">
-      <div><img src={picture} alt={`${name}'s profile picture`} /></div>
-      <div>{name}</div>
-      <div>{location}</div>
-      <div>Phone {phone}</div>
-      <div>Email {email}</div>
-      <hr />
+    <div className="user__card">
+      <div className="user__top">
+        <img className="user__photo" src={picture} alt={`${name}'s profile picture`} />
+        <div className="user__data">
+          <div className="user__name">{name}</div>
+          <div className="user__location">{location}</div>
+        </div>
+        <button className="accordion__button">
+          <img className="accordion__arrow" src={arrow} alt="Show contact info"/>
+        </button>
+      </div>
+
+      <div className="accordion">
+        <div>Phone {phone}</div>
+        <div>Email {email}</div>
+      </div>
     </div>
   );
 }
